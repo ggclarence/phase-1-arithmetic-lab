512ed5231f6212dc5c113dbf9e2c90a99c6ad200
Eric Van <ggclarence@yahoo.com>
2022-02-01 09:53:48 -0600

    commit tree e61270947ba51098af6cc6797bf1fd1ca6df3d6f

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+// simple multiply
+let num1 = 31
+let num2 = 2
+
+let multiply = num1 * num2
+
+// Random number 1-10
+let random = Math.ceil(Math.random() * 10) + 1;
+
+// remainder equal to 4
+let num3 = 12
+let num4 = 3
+let mod = num3/num4
+
+// Maximum number is 20
+
+
+let max = Math.max(1,2,4,5,20)
